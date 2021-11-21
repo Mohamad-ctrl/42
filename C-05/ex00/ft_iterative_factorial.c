f51f741c8d24a1edff46b5de0927a24fbe291561
Mohamad <69107090+Mohamad-ctrl@users.noreply.github.com>
2021-11-21 16:39:45 +0400

    BSQ

---
+++ C-05/ex00/ft_iterative_factorial.c
@@ -0,0 +1,18 @@
+int        ft_iterative_factorial(int nb)
+{
+        int        r;
+        int        x;
+
+        x = 1;
+        r = 1;
+        if ((nb <= 0) || (nb > 12))
+                return (0);
+        if (nb == 1)
+                return (1);
+        while (x <= nb)
+        {
+                r = x * r;
+                x++;
+        }
+        return (r);
+}
